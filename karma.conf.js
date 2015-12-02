76d5acfb2d89c2c95c3b8f72eaa5fdd6fc7adde1
tsuyoshiwada <v23.b.wada@gmail.com>
2015-12-02 23:49:53 +0900

    Update setup module

---
+++ karma.conf.js
@@ -0,0 +1,29 @@
+module.exports = function(config) {
+  config.set({
+    basePath: "",
+    frameworks: ["mocha", "browserify", "fixture"],
+    files: [
+      "test/**/*.spec.js",
+      "test/fixtures/**/*.html"
+    ],
+    exclude: [],
+    preprocessors: {
+      "test/**/*.spec.js": "browserify",
+      "test/**/*.html": "html2js"
+    },
+    browserify: {
+      debug: true,
+      transform: [
+        ["babelify", {plugins: ["babel-plugin-espower"]}]
+      ]
+    },
+    reporters: ["progress"],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ["PhantomJS"],
+    singleRun: false,
+    concurrency: Infinity
+  })
+}
