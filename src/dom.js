b102cd7652bf7815bd426f3c476ccfd948bf462e
tsuyoshiwada <v23.b.wada@gmail.com>
2015-12-05 22:31:25 +0900

    Update dom utils

--- src/dom.js
+++ src/dom.js
@@ -1,4 +1,15 @@
 import * as Util from "./utils"
+import {$$} from "./selectors";
+
+const directionMethodMap = {
+  y: "scrollTop",
+  x: "scrollLeft"
+};
+
+const directionPropMap = {
+  y: "pageYOffset",
+  x: "pageXOffset"
+};
 
 export function isRootContainer(el) {
   const doc = document;
@@ -11,12 +22,63 @@ export function setAttributes(el, props) {
   });
 }
 
-export function getScroll(el, direction = "x") {
-  // @TODO
+function getScrollable(selectors, direction = "y", all = true) {
+  const method = directionMethodMap[direction];
+  const elements = $$(selectors);
+  const scrollables = [];
+
+  for ( let i = 0; i < elements.length; i++ ) {
+    let el = elements[i];
+
+    if( el[method] > 0 ) {
+      scrollables.push(el);
+
+    } else {
+      el[method] = 1;
+      if( el[method] > 0 ) {
+        scrollables.push(el);
+      }
+      el[method] = 0;
+    }
+
+    if ( !all && scrollables.length > 0 ) break;
+  }
+
+  return scrollables;
 }
 
-export function setScroll(el, offset, direction = "x") {
-  // @TODO
+export function scrollableFindAll(selectors, direction) {
+  return getScrollable(selectors, direction, true);
+}
+
+export function scrollableFind(selectors, direction) {
+  const scrollables = getScrollable(selectors, direction, false);
+  return scrollables.length >= 1 ? scrollables[0] : undefined;
+}
+
+function getWindow(el) {
+  return (el != null && el === el.window) ? el : el.nodeType === 9 && el.defaultView;
+}
+
+export function getScroll(el, direction = "y") {
+  const method = directionMethodMap[direction];
+  const prop = directionPropMap[direction];
+  const win = getWindow(el);
+  return win ? win[prop] : el[method];
+}
+
+export function setScroll(el, offset, direction = "y") {
+  const method = directionMethodMap[direction];
+  const win = getWindow(el);
+  const top = direction === "y";
+  if (win) {
+    win.scrollTo(
+      !top ? offset : win.pageXOffset,
+      top ? offset : win.pageYOffset
+    );
+  } else {
+    el[method] = offset;
+  }
 }
 
 export function getOffset(el, context = null) {
