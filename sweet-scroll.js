756e476a904194398f11dbbd7235241601bdf0c5
tsuyoshiwada <v23.b.wada@gmail.com>
2016-03-13 19:48:14 +0900

    release 0.2.5

--- sweet-scroll.js
+++ sweet-scroll.js
@@ -5,7 +5,7 @@
  * @author tsuyoshiwada
  * @homepage https://github.com/tsuyoshiwada/sweet-scroll
  * @license MIT
- * @version 0.2.4
+ * @version 0.2.5
  */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
