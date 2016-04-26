18c894a4a6713418abae06480ee33d6386bad2ff
tsuyoshiwada <v23.b.wada@gmail.com>
2016-04-26 10:32:33 +0900

    Update banner

--- sweet-scroll.js
+++ sweet-scroll.js
@@ -1,12 +1,12 @@
+
 /*!
  * sweet-scroll
  * Modern and the sweet smooth scroll library.
- * 
  * @author tsuyoshiwada
- * @homepage https://github.com/tsuyoshiwada/sweet-scroll
  * @license MIT
  * @version 0.7.1
  */
+
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
